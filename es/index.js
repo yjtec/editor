2a5f3b43d630307c8d1254c9234e95d0336f532c
kidkang <723616880@qq.com>
2019-10-16 09:04:37 +0800

    .0.0.4

--- es/index.js
+++ es/index.js
@@ -18,6 +18,7 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 import React, { Component } from 'react';
 import BraftEditor from 'braft-editor';
+import request from 'umi-request';
 import 'braft-editor/dist/index.css';
 
 var Editor =
@@ -38,6 +39,77 @@ function (_Component) {
         outputHTML: editorState.toHTML()
       }, function () {
         _this.props.onChange(_this.state.outputHTML);
+      });
+    };
+
+    _this.myUploadFn = function (param) {
+      var serverURL = _this.props.action;
+      var xhr = new XMLHttpRequest();
+      var fd = new FormData();
+
+      var successFn = function successFn(response) {
+        // 假设服务端直接返回文件上传后的地址
+        // 上传成功后调用param.success并传入上传后的文件地址
+        param.success({
+          url: xhr.responseText,
+          meta: {
+            id: 'xxx',
+            title: 'xxx',
+            alt: 'xxx',
+            loop: true,
+            // 指定音视频是否循环播放
+            autoPlay: true,
+            // 指定音视频是否自动播放
+            controls: true,
+            // 指定音视频是否显示控制栏
+            poster: 'http://xxx/xx.png' // 指定视频播放器的封面
+
+          }
+        });
+      };
+
+      var progressFn = function progressFn(event) {
+        // 上传进度发生变化时调用param.progress
+        param.progress(event.loaded / event.total * 100);
+      };
+
+      var errorFn = function errorFn(response) {
+        // 上传发生错误时调用param.error
+        param.error({
+          msg: 'unable to upload.'
+        });
+      };
+
+      xhr.upload.addEventListener("progress", progressFn, false);
+      xhr.addEventListener("load", successFn, false);
+      xhr.addEventListener("error", errorFn, false);
+      xhr.addEventListener("abort", errorFn, false);
+      fd.append('file', param.file);
+      fd.append('type', 'store_avatar');
+      xhr.open('POST', serverURL, true);
+      xhr.send(fd);
+    };
+
+    _this.uploadFn = function (_ref) {
+      var file = _ref.file,
+          success = _ref.success;
+      var _this$props = _this.props,
+          action = _this$props.action,
+          data = _this$props.data;
+      var formData = new FormData();
+      formData.append('file', file);
+
+      if (data) {
+        Object.keys(data).map(function (key) {
+          formData.append(key, data[key]);
+        });
+      }
+
+      request(action, {
+        method: 'post',
+        data: formData
+      }).then(function (re) {
+        success(re.data);
       });
     };
 
@@ -70,7 +142,10 @@ function (_Component) {
         }
       }, React.createElement(BraftEditor, {
         value: editorState,
-        onChange: this.handleChange
+        onChange: this.handleChange,
+        media: {
+          uploadFn: this.uploadFn
+        }
       }));
     }
   }]);
@@ -79,6 +154,8 @@ function (_Component) {
 }(Component);
 
 Editor.defaultProps = {
-  onChange: function onChange() {}
+  onChange: function onChange() {},
+  action: '/',
+  data: {}
 };
 export default Editor;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import BraftEditor from 'braft-editor';
+import request from 'umi-request';
 import 'braft-editor/dist/index.css';
 class Editor extends Component{
   constructor(props) {
@@ -25,6 +26,68 @@ class Editor extends Component{
       this.props.onChange(this.state.outputHTML)
     })
   }
+  myUploadFn = (param) => {
+    const {action:serverURL} = this.props;
+    const xhr = new XMLHttpRequest
+    const fd = new FormData()
+
+    const successFn = (response) => {
+      // 假设服务端直接返回文件上传后的地址
+      // 上传成功后调用param.success并传入上传后的文件地址
+      param.success({
+        url: xhr.responseText,
+        meta: {
+          id: 'xxx',
+          title: 'xxx',
+          alt: 'xxx',
+          loop: true, // 指定音视频是否循环播放
+          autoPlay: true, // 指定音视频是否自动播放
+          controls: true, // 指定音视频是否显示控制栏
+          poster: 'http://xxx/xx.png', // 指定视频播放器的封面
+        }
+      })
+    }
+
+    const progressFn = (event) => {
+      // 上传进度发生变化时调用param.progress
+      param.progress(event.loaded / event.total * 100)
+    }
+
+    const errorFn = (response) => {
+      // 上传发生错误时调用param.error
+      param.error({
+        msg: 'unable to upload.'
+      })
+    }
+
+    xhr.upload.addEventListener("progress", progressFn, false)
+    xhr.addEventListener("load", successFn, false)
+    xhr.addEventListener("error", errorFn, false)
+    xhr.addEventListener("abort", errorFn, false)
+
+    fd.append('file', param.file);
+    fd.append('type', 'store_avatar');
+    xhr.open('POST', serverURL, true)
+    xhr.send(fd);
+
+  }
+
+  uploadFn = ({file,success}) => {
+    const {action,data} = this.props;
+    const formData = new FormData();
+    formData.append('file', file);
+    if(data){
+      Object.keys(data).map(key => {
+        formData.append(key,data[key]);
+      })
+    }
+    request(action,{
+      method:'post',
+      data:formData
+    }).then(re => {
+      success(re.data)
+    })
+  }
   render(){
     const {editorState,outputHTML} = this.state;
     return (
@@ -32,12 +95,15 @@ class Editor extends Component{
         <BraftEditor
           value={editorState}
           onChange={this.handleChange}
+          media={{uploadFn:this.uploadFn}}
         />
       </div>
     )
   }
 }
 Editor.defaultProps = {
-  onChange:()=>{}
+  onChange:()=>{},
+  action:'/',
+  data:{}
 }
 export default Editor;
